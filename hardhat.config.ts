26f820c4b023209f3b4ad27c42ff3aa4f4ff732f
Amit Yadav <amy29981@gmail.com>
2023-12-09 00:07:10 +0530

    import anon

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  dependencyCompiler: {
+    paths: ["anon-aadhaar-contracts/contracts/Verifier.sol"],
+  },
+  // networks: {
+  //   goerli: {
+  //     url: "https://ethereum-goerli.publicnode.com",
+  //     accounts: [process.env.PRIVATE_KEY || ""],
+  //   },
+  // },
 };
 
 export default config;
