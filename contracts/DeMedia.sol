a8d3d59e3c25ff55ae396946bad5660822c23349
Amit Yadav <amy29981@gmail.com>
2023-12-09 09:11:50 +0530

    add events

--- contracts/DeMedia.sol
+++ contracts/DeMedia.sol
@@ -17,7 +17,8 @@ contract DeMedia {
     address public anonAadhaarVerifierAddr;
     uint256 public mediaCounter;
 
-    event Voted(address indexed _from, uint256 indexed _propositionIndex);
+    event Voted(address indexed _from, uint256 indexed _mediaIndex);
+    event Created(address indexed _from, uint256 indexed _mediaIndex);
 
     // List of media
     Media[] public medias;
@@ -25,6 +26,7 @@ contract DeMedia {
     // Constructor to initialize proposals
     constructor(address _verifierAddr) {
         anonAadhaarVerifierAddr = _verifierAddr;
+        mediaCounter = 0;
     }
 
     function verify(uint256[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[34] calldata _pubSignals) public view returns (bool) {
@@ -32,6 +34,17 @@ contract DeMedia {
     }
 
     // Function to add media
+    function addMedia(string description, uint256[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[34] calldata _pubSignals) public {
+        // Needs to be an anon-verified user
+        require(verify(_pA, _pB, _pC, _pubSignals), "Your idendity proof is not valid");
+
+        mediaCounter++;
+        medias.push(Media(description, 0, 0, 0, 0));
+
+        emit Created(msg.sender, mediaCounter);
+    }
+
+    // Function to vote on media
     function voteForProposal(uint256 proposalIndex, uint256[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[34] calldata _pubSignals) public {
         require(proposalIndex < proposals.length, "Invalid proposal index");
         require(!hasVoted[msg.sender], "You have already voted");
