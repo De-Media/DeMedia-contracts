c9996ada8a37774753104bc7bf06c3ca4f92d5be
Amit Yadav <amy29981@gmail.com>
2023-12-10 10:35:18 +0530

    testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,9 +16,13 @@ const config: HardhatUserConfig = {
       accounts: [process.env.PRIVATE_KEY || ""],
       gasPrice: 35000000000,
     },
-    // scroll: {
-
-    // },
+    scroll: {
+      url: "https://sepolia-rpc.scroll.io",
+      //  url: "https://rpc-mumbai.matic.today",
+      // url: "https://rpc-mumbai.maticvigil.com",
+       accounts: [process.env.PRIVATE_KEY || ""],
+       //gasPrice: 35000000000,
+    },
     // okx: {
 
     // },
@@ -26,6 +30,21 @@ const config: HardhatUserConfig = {
       url: "https://ethereum-goerli.publicnode.com",
       accounts: [process.env.PRIVATE_KEY || ""],
     },
+  },
+  etherscan: {
+    apiKey: {
+      scroll: 'U68J8UHTQBQ2Q642F1MAR1G7DNI8MS9V96',
+    },
+    customChains: [
+      {
+        network: 'scroll',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://sepolia-blockscout.scroll.io/api',
+          browserURL: 'https://sepolia-blockscout.scroll.io/',
+        },
+      },
+    ],
   },
 };
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,30 +2,30 @@ import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
 async function main() {
-    let Verifier = await ethers.getContractFactory("Verifier");
-    let verifier = await Verifier.deploy();
-    let appIdBigInt = "196700487049306364386084600156231018794323017728";
+    // let Verifier = await ethers.getContractFactory("Verifier");
+    // let verifier = await Verifier.deploy();
+    // let appIdBigInt = "196700487049306364386084600156231018794323017728";
 
-    await verifier.waitForDeployment();
-    const _verifierAddress = verifier.getAddress();
+    // await verifier.waitForDeployment();
+    // const _verifierAddress = verifier.getAddress();
 
-    console.log(_verifierAddress, "HERE----->>");
+    // console.log(_verifierAddress, "HERE----->>");
 
-    const AnonAadhaarVerifier = await ethers.getContractFactory(
-      "AnonAadhaarVerifier"
-    );
-    const anonAadhaarVerifier = await AnonAadhaarVerifier.deploy(
-      _verifierAddress,
-      appIdBigInt
-    );
+    // const AnonAadhaarVerifier = await ethers.getContractFactory(
+    //   "AnonAadhaarVerifier"
+    // );
+    // const anonAadhaarVerifier = await AnonAadhaarVerifier.deploy(
+    //   _verifierAddress,
+    //   appIdBigInt
+    // );
 
-    await anonAadhaarVerifier.waitForDeployment();
-    const _AnonAadhaarVerifierAddress = anonAadhaarVerifier.getAddress();
-    console.log(_AnonAadhaarVerifierAddress, "HERE----->>");
+    // await anonAadhaarVerifier.waitForDeployment();
+    // const _AnonAadhaarVerifierAddress = anonAadhaarVerifier.getAddress();
+    // console.log(_AnonAadhaarVerifierAddress, "HERE----->>");
 
     let DeMedia = await ethers.getContractFactory("DeMedia");
     let demedia = await DeMedia.deploy(
-      _AnonAadhaarVerifierAddress
+      '0x6a5Ad93f028528840014Fb2291378bCbe021962e'
     );
 
     await demedia.waitForDeployment();
